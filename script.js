85284a21a2eb1aea4a1f2a249a6163d68f91e03d
khushi_sukar <128124987+Khushi-Sk@users.noreply.github.com>
2023-10-27 18:05:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ const countObject = {};
 
 let url = 'https://one00x-data-analysis.onrender.com/assignment?email=khushisukar234@gmail.com'
 
+const getData = async(url) => {
     try {
-        fetch(url, {
+        const response = await fetch(url, {
             method:'GET',
             body: JSON.stringify()
         })
-        .then(response => response.json())    
-        .then(data => {
-            // console.log(data)
-            data.forEach(element => {
-                if (countObject[element]) {
-                    countObject[element]++;
-                } else {
-                    countObject[element] = 1;
-                }
-            });
 
+        if (response.ok) {
+                dataId = response.headers.get('x-assignment-id');
+                // console.log(response.status)
+                // console.log(dataId)
+            const data = await response.json();
+                // console.log(data)
+                data.forEach(element => {
+                    if (countObject[element]) {
+                        
+                        countObject[element]++;
+                    } else {
+                        countObject[element] = 1;
+                    }
+                })
+        };
+
+        // console.log(countObject)
         let mostCommonString;
         let mostCommonCount = 0;
 
@@ -27,11 +35,29 @@ let url = 'https://one00x-data-analysis.onrender.com/assignment?email=khushisuka
                 mostCommonString = count 
             }   
         }
-        
-            console.log(`The most common word is "${mostCommonString}" and it's ${mostCommonCount} times.`)
-    }) .catch((error) => {
-        console.log(`Error: ${error}`)
-    })
-    } catch(error) {
-        console.log(`Error: ${error}`)
-    }
+        // console.log(`The most common word is "${mostCommonString}" and it's ${mostCommonCount} times.`)
+
+        // post method:
+        try{
+            const postResponse = await fetch(url, {
+                method:'POST',
+                body: JSON.stringify({
+                    assignment_id: dataId,
+                    answer: mostCommonString,
+                }),
+                headers: {'Content-Type': 'application/json'}
+            })
+            postData = await postResponse.json()
+           console.log(postData)
+        } catch (error) {
+            console.log(error)
+        }
+    }   catch(error) {
+            console.log(`Error: ${error}`)
+        }
+}
+
+getData(url)
+
+
+
